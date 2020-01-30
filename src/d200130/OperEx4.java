0266b91b90eb50e0f8073f635f7ad482d5910437
inegg <inegg.apps@gmail.com>
2020-01-30 11:32:46 +0900

    관계 연산자와 동등 연산자 실습

---
+++ src/d200130/OperEx4.java
@@ -0,0 +1,15 @@
+package d200130;
+
+public class OperEx4 {
+	public static void main(String[] args) {
+		int a,b;
+		boolean c;
+		
+		a=10; b=5;
+		c=a>b;
+		System.out.println(c);//true
+		System.out.println(a<b);//false
+		System.out.println(a==b);//false
+		System.out.println(a!=b);//true
+	}
+}
