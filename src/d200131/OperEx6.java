623a97b238d4261f3737912b691760c4887be101
inegg <inegg.apps@gmail.com>
2020-01-31 10:14:58 +0900

    삼항연산자를 이용하여 짝수인지 홀수인지 판별하는 예제 실습

---
+++ src/d200131/OperEx6.java
@@ -0,0 +1,20 @@
+package d200131;
+
+import java.util.Scanner;
+
+public class OperEx6 {
+	public static void main(String[] args) {
+		// 정수를 입력 받아 짝수인지 홀수인지를 판별
+		Scanner sc = new Scanner(System.in);
+		int a;
+		String s;
+
+		System.out.print("정수 입력: ");
+		a = sc.nextInt();
+
+		s = a % 2 == 0 ? "짝수" : "홀수";
+		System.out.println(a + "은 " + s + "입니다.");
+
+		sc.close();
+	}
+}
