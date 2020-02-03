aa4fac04a7cfc57ed5b3f0ee888b3fc6c4566e84
inegg <inegg.apps@gmail.com>
2020-02-03 12:40:10 +0900

    자바의정석 p150 예제 실습 (3rd edition)

---
+++ src/r200203/FlowEx7.java
@@ -0,0 +1,35 @@
+package r200203;
+
+import java.util.Scanner;
+
+public class FlowEx7 {
+	public static void main(String[] args) {
+		System.out.print("가위, 바위, 보 중 하나를 입력하세요.> ");
+		
+		Scanner sc = new Scanner(System.in);
+		int user = sc.nextInt();
+		int com = (int) (Math.random()*3)+1;
+		
+		System.out.println("당신은 " + user +"입니다.");
+		System.out.println("컴퓨터는 " + com+"입니다.");
+		
+		switch(user-com) {
+		case 2: case -1:
+			//가위(1)-바위(2)=-1
+			//바위(2)-보(3)=-1
+			//보(3)-가위(1)=2
+			System.out.println("당신이 졌습니다.");
+			break;
+		case 1: case -2:
+			//가위(1)-보(3)=-2
+			//바위(2)-가위(1)=1
+			//보(3)-바위(2)=1
+			System.out.println("당신이 이겼습니다.");
+			break;
+		case 0:
+			System.out.println("무승부.");
+			break;
+		}
+		sc.close();
+	}
+}
