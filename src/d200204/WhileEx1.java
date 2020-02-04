d646b9296c451007de8e95406787a932f3809478
inegg <inegg.apps@gmail.com>
2020-02-04 09:13:22 +0900

    While문과 DoWhile문과의 차이를 살펴보는 예제

--- src/d200204/WhileEx1.java
+++ src/d200204/WhileEx1.java
@@ -8,14 +8,28 @@ public class WhileEx1 {
 //			System.out.println("안: "+n);
 //		}while(n<10);
 //		System.out.println("밖: "+n);
-		
-		int n, s;
-		n=s=0;
-		
+
+//		int n, s;
+//		n=s=0;
+//		
+//		do {
+//			n++;
+//			s+=n;
+//		}while(n<10);
+//		System.out.println("결과: " + s);
+
+		int n = 10;
+		while (n < 10) {
+			n++;
+			System.out.println("while 안: " + n);
+		}
+		System.out.println("while 밖: " + n);
+
+		n = 10;
 		do {
 			n++;
-			s+=n;
-		}while(n<10);
-		System.out.println("결과: " + s);
+			System.out.println("do~while 안: " + n);
+		} while (n < 10);
+		System.out.println("do~while 밖: " + n);
 	}
 }
