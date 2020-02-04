54e6ea8f288575810e1d585afc30e9d800527766
inegg <inegg.apps@gmail.com>
2020-02-04 09:07:53 +0900

    DoWhile문 사용 예제

---
+++ src/d200204/WhileEx1.java
@@ -0,0 +1,12 @@
+package d200204;
+
+public class WhileEx1 {
+	public static void main(String[] args) {
+		int n=0;
+		do {
+			n++;
+			System.out.println("¾È: "+n);
+		}while(n<10);
+		System.out.println("¹Û: "+n);
+	}
+}
