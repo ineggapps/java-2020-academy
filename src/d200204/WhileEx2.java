d7fab9b26888097b0549037d5803f13d358ac138
inegg <inegg.apps@gmail.com>
2020-02-04 09:32:56 +0900

    do~while문을 배제하여 똑같은 기능 구현해 보기

--- src/d200204/WhileEx2.java
+++ src/d200204/WhileEx2.java
@@ -6,12 +6,21 @@ import java.util.Scanner;
 public class WhileEx2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int dan, n=0;
-		do {
+		int dan=1, n=0;
+		boolean b=true;
+		/*
+		do~while문을 이용하지 않고도 얼마든지 작성은 가능하지만
+		반복문을 처음에 진입하기 위해 boolean 변수를 추가로 true로 초기화하여 설정하여 진입하도록 만들어야 한다.
+		가독성을 따지자면 do~while문이 좋다.
+		*/
+		while(b){
 			System.out.print("단 입력> ");
 			dan = sc.nextInt();
-			//while의 조건은 입력을 다시 받는 조건을 명시하면 된다.
-		}while(dan<1 || dan>9);
+			//조건식을 판별하여 1~9까지의 숫자라는 조건을 만족하면 b=false로 설정하여 반복문을 빠져나간다.
+			if(dan>=1 && dan<=9) {
+				b=false;
+			}
+		}
 		
 		while(n<9) {
 			n++;
