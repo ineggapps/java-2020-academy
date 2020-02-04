ac26ee734b32ccace7cd1521adf991f99a76485c
inegg <inegg.apps@gmail.com>
2020-02-04 09:16:27 +0900

    주석문 추가

--- src/d200204/WhileEx1.java
+++ src/d200204/WhileEx1.java
@@ -19,14 +19,14 @@ public class WhileEx1 {
 //		System.out.println("결과: " + s);
 
 		int n = 10;
-		while (n < 10) {
+		while (n < 10) {//처음 조건을 만족하지 않으면 한 번도 실행하지 않는다.
 			n++;
 			System.out.println("while 안: " + n);
 		}
 		System.out.println("while 밖: " + n);
 
 		n = 10;
-		do {
+		do {//조건을 밑에서 비교하므로 적어도 한 번은 실행한다.
 			n++;
 			System.out.println("do~while 안: " + n);
 		} while (n < 10);
