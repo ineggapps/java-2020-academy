3dec21e1fa847f759d4e2efde81e62a720763dd4
inegg <inegg.apps@gmail.com>
2020-02-04 09:10:28 +0900

    1에서 10까지의 합계 구하는 프로그램 작성

--- src/d200204/WhileEx1.java
+++ src/d200204/WhileEx1.java
@@ -2,11 +2,20 @@ package d200204;
 
 public class WhileEx1 {
 	public static void main(String[] args) {
-		int n=0;
+//		int n=0;
+//		do {
+//			n++;
+//			System.out.println("안: "+n);
+//		}while(n<10);
+//		System.out.println("밖: "+n);
+		
+		int n, s;
+		n=s=0;
+		
 		do {
 			n++;
-			System.out.println("안: "+n);
+			s+=n;
 		}while(n<10);
-		System.out.println("밖: "+n);
+		System.out.println("결과: " + s);
 	}
 }
