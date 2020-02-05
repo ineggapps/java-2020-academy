eeb1f95bb3224f74ecd3513ca632d65d549ae836
inegg <inegg.apps@gmail.com>
2020-02-05 09:44:42 +0900

    For문을 While문으로 변환하기 다중 반복문을 이용하여 다음의 숫자 출력하기
      1   2   3   4   5   6   7   8   9  10
      2   3   4   5   6   7   8   9  10  11
      3   4   5   6   7   8   9  10  11  12
      4   5   6   7   8   9  10  11  12  13
      5   6   7   8   9  10  11  12  13  14
      6   7   8   9  10  11  12  13  14  15
      7   8   9  10  11  12  13  14  15  16
      8   9  10  11  12  13  14  15  16  17
      9  10  11  12  13  14  15  16  17  18
     10  11  12  13  14  15  16  17  18  19

---
+++ src/d200205/ForEx5.java
@@ -0,0 +1,25 @@
+package d200205;
+
+public class ForEx5 {
+	public static void main(String[] args) {
+		//for문을 while문으로 변환하여 작성하기 예제
+		for(int i=1;i<=10;i++) {
+			for(int j=i;j<=i+9;j++) {
+				System.out.printf("%3d\t",j);
+			}
+			System.out.println();
+		}
+		//Mini quiz
+		System.out.println("======================");
+		int i=1,j;		
+		while(i<=10) {
+			j=i;
+			while(j<=i+9) {
+				System.out.printf("%3d\t",j);
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+	}
+}
