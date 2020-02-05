84697dcf4264980437b0214a26ada500c2d87caa
inegg <inegg.apps@gmail.com>
2020-02-05 15:47:39 +0900

    5번 응용문제 답안 개선

---
+++ src/d200205/ForQuizHard5Answer.java
@@ -0,0 +1,33 @@
+package d200205;
+
+public class ForQuizHard5Answer {
+	public static void main(String[] args) {
+		// 1~100까지의 수 중 소수를 한 줄에 10개씩출력하고 마지막에 소수의 개수를 출력하는 프로그램
+		int cnt = 0;
+		boolean b = true;
+		for (int i = 2; i <= 100; i++) {
+			b = false;
+			// 단, 수학에서 1은 소수가 아니므로 2부터 반복을 시작한다.
+			for (int j = 2; j < i; j++) {
+				// j는 i가 소수인지 판별하기 위해 2부터 자기자신까지 증가하며 순차적으로 대입해 나눠 본다.
+				if (i % j == 0) {// i는 j와 나누어 떨어지고, 자기 자신이 아니면?
+					// 조건을 만족하면 어차피 소수가 아니므로 연산을 중단한다
+					b = true;
+					break;
+				}
+			}
+			if (!b) {
+				System.out.printf("%d\t", i);
+				cnt++;
+				if (cnt % 10 == 0) {
+					System.out.println();
+				}
+			}
+		}
+		System.out.println("\n소수의 개수: " + cnt);
+
+		/*
+		 * 심화: 123의 약수를 구할 때 2~12까지만 계산하여도 소수임을 단정할 수 있다. ∵ 12^2=144 이다.
+		 */
+	}
+}
