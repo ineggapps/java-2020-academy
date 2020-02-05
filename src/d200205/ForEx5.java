bc69ee809a9e577e1f54dcc6a24a4b625244a441
inegg <inegg.apps@gmail.com>
2020-02-05 10:12:20 +0900

    beautify

--- src/d200205/ForEx5.java
+++ src/d200205/ForEx5.java
@@ -2,20 +2,20 @@ package d200205;
 
 public class ForEx5 {
 	public static void main(String[] args) {
-		//for문을 while문으로 변환하여 작성하기 예제
-		for(int i=1;i<=10;i++) {
-			for(int j=i;j<=i+9;j++) {
-				System.out.printf("%3d\t",j);
+		// for문을 while문으로 변환하여 작성하기 예제
+		for (int i = 1; i <= 10; i++) {
+			for (int j = i; j <= i + 9; j++) {
+				System.out.printf("%3d\t", j);
 			}
 			System.out.println();
 		}
-		//Mini quiz
+		// Mini quiz
 		System.out.println("======================");
-		int i=1,j;		
-		while(i<=10) {
-			j=i;
-			while(j<=i+9) {
-				System.out.printf("%3d\t",j);
+		int i = 1, j;
+		while (i <= 10) {
+			j = i;
+			while (j <= i + 9) {
+				System.out.printf("%3d\t", j);
 				j++;
 			}
 			System.out.println();
