dc4eb348b0b179070aad26fc9987133b5d24e8e4
inegg <inegg.apps@gmail.com>
2020-02-05 15:53:58 +0900

    break; 쓰고 안 쓰고의 퍼포먼스 차이

--- src/d200205/ForQuizHard5Answer.java
+++ src/d200205/ForQuizHard5Answer.java
@@ -5,7 +5,8 @@ public class ForQuizHard5Answer {
 		// 1~100까지의 수 중 소수를 한 줄에 10개씩출력하고 마지막에 소수의 개수를 출력하는 프로그램
 		int cnt = 0;
 		boolean b = true;
-		for (int i = 2; i <= 100; i++) {
+		long m = System.currentTimeMillis();
+		for (int i = 2; i <= 100000; i++) {
 			b = false;
 			// 단, 수학에서 1은 소수가 아니므로 2부터 반복을 시작한다.
 			for (int j = 2; j < i; j++) {
@@ -13,7 +14,9 @@ public class ForQuizHard5Answer {
 				if (i % j == 0) {// i는 j와 나누어 떨어지고, 자기 자신이 아니면?
 					// 조건을 만족하면 어차피 소수가 아니므로 연산을 중단한다
 					b = true;
-					break;
+					break;//break를 쓰고 안 쓰고 프로그래밍 속도 차이가 크다.
+					//break사용: 1에서 10만까지의 소수를 구할 때는 935ms
+					//break 미사용: 1에서 10만까지의 소수를 구할 때 9104ms
 				}
 			}
 			if (!b) {
@@ -25,7 +28,7 @@ public class ForQuizHard5Answer {
 			}
 		}
 		System.out.println("\n소수의 개수: " + cnt);
-
+		System.out.println(System.currentTimeMillis()-m);
 		/*
 		 * 심화: 123의 약수를 구할 때 2~12까지만 계산하여도 소수임을 단정할 수 있다. ∵ 12^2=144 이다.
 		 */
