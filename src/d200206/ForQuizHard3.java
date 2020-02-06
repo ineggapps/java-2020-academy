dc44c750ff26939b3186c885675212dd661be72d
inegg <inegg.apps@gmail.com>
2020-02-06 11:04:59 +0900

    굳이 cnt변수는 필요가 없다.

--- src/d200206/ForQuizHard3.java
+++ src/d200206/ForQuizHard3.java
@@ -4,7 +4,6 @@ public class ForQuizHard3 {
 	public static void main(String[] args) {
 		// 1. 1~100까지의 수 중 3,6,9가 들어 있는 수는 *로 출력, 나머지는 자신의 수 출력
 		// 2. 한 줄에는 10개씩만 출력
-		int cnt = 0;
 		for (int i = 1; i <= 100; i++) {
 
 			//100까지라고 범위를 한정했으므로 10을 나눈 나머지(일의자리)와 10을 나눈 몫(10의 자리)를 구하여 3,6,9가 들어갔는지를 확인한다.
@@ -14,7 +13,7 @@ public class ForQuizHard3 {
 				System.out.format("%d\t", i);
 			}
 
-			if (++cnt % 10 == 0) {
+			if (i % 10 == 0) {
 				System.out.println();
 			}
 		}
