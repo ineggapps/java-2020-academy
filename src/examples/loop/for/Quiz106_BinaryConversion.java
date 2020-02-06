d5b07123bb512cc9f85540b2ae6364c9efa015af
inegg <inegg.apps@gmail.com>
2020-02-06 17:49:00 +0900

    답안 업로드

---
+++ src/examples/loop/for/Quiz106_BinaryConversion.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Quiz106_BinaryConversion {
+	public static void main(String[] args) {
+/*
+		정수를 입력 받아 2진수로 변환하는 프로그램
+*/
+		Scanner sc=new Scanner(System.in);
+
+		int num;
+		System.out.print("정수 ? ");
+		num = sc.nextInt();
+		
+		System.out.printf("%1$d(%1$#X) => ", num);
+		for(int i = 0; i < 32; i++) {
+			if(i != 0 && i % 4 == 0)
+				System.out.print(" ");
+			System.out.printf("%c", (num << i & 0x80000000)  != 0 ? '1' : '0');
+		}
+		System.out.println();
+		sc.close();
+	}
+}
