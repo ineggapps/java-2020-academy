7e1413e7c7e1c04a2e176ce731a1155868f42af9
inegg <inegg.apps@gmail.com>
2020-02-07 12:40:27 +0900

    주석 추가

--- src/d200207/ArrayQuiz3.java
+++ src/d200207/ArrayQuiz3.java
@@ -17,16 +17,17 @@ public class ArrayQuiz3 {
 		} while (amount < 1 || amount > 5);
 
 		for (int i = 0; i < amount; i++) {
-			System.out.printf("%d번째: ", i + 1);
 
 			// 랜덤하게 섞기
 			for (int j = 0; j < num.length; j++) {
 				num[j] = (int) (Math.random() * 45) + 1;
+				//TODO: 중복된 값이 나올 수 있다. 중복을 어떻게 해결하느냐?
 			}
 			// 정렬하기
 			Arrays.sort(num);
 
 			// 출력하기
+			System.out.printf("%d번째: ", i + 1);
 			for (int j = 0; j < num.length; j++) {
 				System.out.printf("%d\t\t", num[j]);
 			}
