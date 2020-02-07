45d643da52501880adfc2e4dfbaa513ce4852dd6
inegg <inegg.apps@gmail.com>
2020-02-07 10:46:34 +0900

    4380원을 500원, 100원, 50원, 10원짜리로 지불하기 위해서 필요한 동전의 개수를 구하는 프로그램. 배열을 이용하기

---
+++ src/d200207/ArrayEx6.java
@@ -0,0 +1,18 @@
+package d200207;
+
+public class ArrayEx6 {
+	public static void main(String[] args) {
+		/*
+		 * 4380원을 500원, 100원, 50원, 10원짜리로 지불하기 위해서 필요한 동전의 개수를 구하는 프로그램. 배열을 이용하기
+		 */
+		int m = 4380;
+		int[] u = new int[] { 500, 100, 50, 10 };
+		int tmp;
+		System.out.printf("금액: %d\n",m);
+		for (int i = 0; i < u.length; i++) {
+			tmp = m/u[i];
+			System.out.printf("%d원: %d장\n",u[i],tmp);
+			m-=u[i]*tmp;
+		}
+	}
+}
