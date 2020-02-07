b5c106a65714c00039dd65024aade2e093e031fa
inegg <inegg.apps@gmail.com>
2020-02-07 12:13:00 +0900

    배열을 이용하여 만년달력 만들기 성공!

---
+++ src/d200207/ArrayCalendar.java
@@ -0,0 +1,73 @@
+package d200207;
+
+import java.util.Scanner;
+
+public class ArrayCalendar {
+	public static void main(String[] args) {
+		// 달력 만들기
+		Scanner sc = new Scanner(System.in);
+		int y, m, d;
+		int month[] = new int[] { 31, 0, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		String week[] = new String[] { "일", "월", "화", "수", "목", "금", "토" };
+		int tot;//구하고자 하는 월의 전월까지의 경과일을 구하기
+		int startDay;
+		
+		do {
+			System.out.print("연도 > ");
+			y = sc.nextInt();
+		} while (y < 1);
+
+		do {
+			System.out.print("월 > ");
+			m = sc.nextInt();
+		} while (m < 1 || m > 12);
+
+		// 보고자 하는 연도가 윤년인지 구하기 (2월)
+		if (y % 4 == 0 && y % 100 != 0 || y % 400 == 0) {
+			month[1]=29;//윤년이면 2월에 1일을 더 추가한다.
+		}else {
+			month[1]=28;
+		}
+		
+//		달력은 월까지만 구하면 된다.
+//		do {
+//			System.out.print("일 > ");
+//			d = sc.nextInt();
+//		} while (d < 1 || d > month[d - 1]);
+
+		//달력 그리기
+		//달력을 그리기 위해서는 월 시작일의 요일을 계산해야 한다.
+		//구하고자 하는 전년도의 말월 말일까지의 날짜 수 카운트
+		//전년도*365일 + (1*전년도까지 윤년이었던 연도의 개수)
+		tot = (y-1)*365 + ((y-1)/4 - (y-1)/100 + (y-1)/400);
+		//구하고자 하는 연도의 월의 전월까지의 날짜 수 카운트하기
+		for(int i=0;i<m-1;i++) {
+			tot+=month[i];
+		}
+		tot++; //현재 월의 1일을 카운트
+		startDay = tot%7;//시작하는 날의 값
+//		System.out.println(month[m-1]+"(말일)까지 있음");
+//		System.out.println(tot+"시작일은 " + tot%7 +" " + week[tot%7]);
+		System.out.printf("%d년 %d월\n",y,m);
+		for(String i: week) {
+			System.out.printf("%s\t\t",i);
+		}
+		System.out.println("");
+		//달력은 7일 단위로 줄바꿈을 하니까 7은 literal값으로 사용할게!
+		int day=1;
+		int cursor=0;
+		while(day<=month[m-1]) {
+			cursor++;
+			if(cursor<7 && cursor%7<=startDay) {
+				System.out.print("*\t\t");
+			}else {
+				System.out.printf("%d\t\t",day);
+				day++;
+			}
+			if(cursor%7==0) {
+				System.out.println();
+			}
+		}
+		sc.close();
+	}
+}
