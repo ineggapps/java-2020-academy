4c9d9323d98a246410f59a50667d965263831873
inegg <inegg.apps@gmail.com>
2020-02-08 18:07:58 +0900

    arraylist실습

---
+++ src/d200207/ArrayListEx1.java
@@ -0,0 +1,18 @@
+package d200207;
+
+import java.util.ArrayList;
+
+public class ArrayListEx1 {
+	public static void main(String[] args) {
+		ArrayList<E> list1= new ArrayList(10);
+		list1.add(new Integer(5));
+		list1.add(new Integer(4));
+		list1.add(new Integer(0));
+		list1.add(new Integer(2));
+		list1.add(new Integer(0));
+		list1.add(new Integer(1));
+		list1.add(new Integer(3));
+		
+		
+	}
+}
