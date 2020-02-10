a8d9a689b95c014daa269b64e0b854060b10a615
inegg <inegg.apps@gmail.com>
2020-02-10 12:20:16 +0900

    한 줄의 코드를 전개하면 다음과 같다.

--- src/d200210/ArrayQuiz1.java
+++ src/d200210/ArrayQuiz1.java
@@ -10,11 +10,19 @@ public class ArrayQuiz1 {
 		 20	 19	 18	 17	 16	
 		 21	 22	 23	 24	 25	1	
 		 * */
-		int[][] a = new int[5][4];
+		int[][] a = new int[5][5];
 		int n = 0;
 		for (int i = 0; i < a.length; i++) {
 			for (int j = 0; j < a[i].length; j++) {
-				a[i][((i&1)==0)?j:a[i].length-1-j] = ++n;
+//				한 줄로 요약
+//				a[i][((i&1)==0)?j:a[i].length-1-j] = ++n;
+				n++;
+				
+				if(i%2==0) {
+					a[i][j]=n;
+				}else {
+					a[i][a[i].length-1-j]=n;
+				}
 			}
 		}
 
