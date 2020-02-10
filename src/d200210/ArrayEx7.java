e0e5db92e17b2a0cfa972911a9115f31056624dd
inegg <inegg.apps@gmail.com>
2020-02-10 11:44:00 +0900

    2차원 배열의 출력방법 (for문을 이용한 방식 2가지로 나뉨)

---
+++ src/d200210/ArrayEx7.java
@@ -0,0 +1,22 @@
+package d200210;
+
+public class ArrayEx7 {
+	public static void main(String[] args) {
+		int[][] a = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a[i].length; j++) {
+				System.out.printf("%3d\t", a[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("==============");
+
+		for (int[] rows : a) {
+			for (int n : rows) {
+				System.out.printf("%3d\t", n);
+			}
+			System.out.println();
+		}
+	}
+}
