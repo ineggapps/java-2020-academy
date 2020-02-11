dce9721775098dcdfdb22c5d19c706c2a5ff0867
inegg <inegg.apps@gmail.com>
2020-02-11 17:02:42 +0900

    추가 실습

--- src/d200211/Ex7.java
+++ src/d200211/Ex7.java
@@ -10,10 +10,13 @@ public class Ex7 {
 		char c = 'g';
 		System.out.println(c + "를 대문자로 고치면? " + tt.upper(c));
 		System.out.println(c + "는 대문자? " + tt.isUpper(c));
-		int a=1,b=10;
-		result = tt.sum2(a,b);
-		System.out.printf("%d에서 %d까지의 합은 %d입니다.%n",a,b,result);
+		int a = 1, b = 10;
+		result = tt.sum2(a, b);
+		System.out.printf("%d에서 %d까지의 합은 %d입니다.%n", a, b, result);
 		tt.gugu(2);
+		tt.gugu(17);
+		int score = 88;
+		System.out.println(score + "점은 " + tt.grade(score) + "입니다.");
 	}
 }
 
@@ -45,27 +48,57 @@ class Test7 {
 
 	// 두 정수를 매개변수로 전달받아 작은 수에서 큰 수까지 합 구하기
 	public int sum2(int a, int b) {
-		int t, s=0;
-		//a가 작은 수가 되도록 정렬하기
+		int t, s = 0;
+		// a가 작은 수가 되도록 정렬하기
 		if (a > b) {
 			t = a;
 			a = b;
 			b = t;
 		}
 		for (int i = a; i <= b; i++) {
-			s+=i;
+			s += i;
 		}
 		return s;
 	}
 
-	//매개변수를 입력 받아서 해당하는 수의 구구단표를 출력
+	// 매개변수를 입력 받아서 해당하는 수의 구구단표를 출력
 	public void gugu(int dan) {
-		System.out.println(dan+"단 출력");
-		for(int i=1;i<=9;i++) {
-			System.out.printf("%d X %d = %2d%n",dan,i,dan*i);
+		if(dan<1 || dan>9){
+			System.out.println("1~9단까지만 입력이 가능합니다");
+			return;
 		}
-		return; //return type이 void형인 경우에는 생략이 가능하다
-		//return type이 void형인 경우에는 메서드를 실행한 후 호출한 곳으로 돌아간다.
-		//값을 넘겨줄 일이 없는 경우에 void형을 이용하여 메서드를 작성한다.
+		System.out.println(dan + "단 출력");
+		for (int i = 1; i <= 9; i++) {
+			System.out.printf("%d X %d = %2d%n", dan, i, dan * i);
+		}
+		return; // return type이 void형인 경우에는 생략이 가능하다
+		// return type이 void형인 경우에는 메서드를 실행한 후 호출한 곳으로 돌아간다.
+		// 값을 넘겨줄 일이 없는 경우에 void형을 이용하여 메서드를 작성한다.
+	}
+
+	// 파라미터로 전달 받은 점수를 이용하여 평점 계산
+	// 단, 입력받은 점숫값에는 오류가 없다는 가정 하에 작성한다.
+	public double grade(int s) {
+		double n;
+		if (s >= 95) {// if(s>=95 && s<=100) 에서 점수가 오류가 없다고 가정하였으므로 s<=100의 조건식은 작성하지 않는다
+			n = 4.5;
+		} else if (s >= 90) {
+			n = 4.0;
+		} else if (s >= 85) {
+			n = 3.5;
+		} else if (s >= 80) {
+			n = 3.0;
+		} else if (s >= 75) {
+			n = 2.5;
+		} else if (s >= 70) {
+			n = 2.0;
+		} else if (s >= 65) {
+			n = 1.5;
+		} else if (s >= 60) {
+			n = 1.0;
+		} else {
+			n = 0;
+		}
+		return n;
 	}
 }
