469f9926d4e741462d2e4da0543e0cfe6646b2cc
inegg <inegg.apps@gmail.com>
2020-02-19 12:16:28 +0900

    super 키워드를 통한 슈퍼 클래스의 메서드 호출

--- src/d200219/Ex6.java
+++ src/d200219/Ex6.java
@@ -3,8 +3,9 @@ package d200219;
 public class Ex6 {
 	public static void main(String[] args) {
 		Test6 ob = new Test6();
-		ob.print();
-
+//		ob.print();
+		
+		ob.fun();
 	}
 }
 
@@ -14,13 +15,19 @@ class Demo6 {
 	public void print() {
 		System.out.println(a);
 	}
+	
 }
 
 class Test6 extends Demo6 {
 	int x = 100;
 
 	@Override // 실행 시까지 생명주기를 나타낼 수 있음.
-	public void print() {
+	public void print() {// 2)
 		System.out.println(a + ":" + x);
+	}
+
+	public void fun() {
+		print(); // 2) 주석이 있는 print() 메서드를 호출한다.
+		super.print();
 	}
 }
