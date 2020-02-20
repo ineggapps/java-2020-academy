03bbf4ca5747ed93170602646d6e8424439b2949
inegg <inegg.apps@gmail.com>
2020-02-20 12:32:27 +0900

    Generic 사용 예제

---
+++ src/d200220/BoxTest.java
@@ -0,0 +1,53 @@
+package d200220;
+
+public class BoxTest {
+	public static void main(String[] args) {
+		Box b = new Box();
+		b.setItem(new Item("H"));
+		
+		BoxGeneric<Item> bb = new BoxGeneric<Item>();
+		bb.setItem(new Item("H"));
+//		bb.setItem(new String("a")); 이런 건 안 된단 말이지!
+	}
+}
+
+class Item {
+	private String name;
+	
+	Item(String name){
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
+
+class Box {
+	Object item;
+
+	void setItem(Object item) {
+		this.item = item;
+	}
+
+	Object getItem() {
+		return item;
+	}
+}
+
+class BoxGeneric<T> {
+	T item;
+
+	void setItem(T item) {
+		this.item = item;
+	}
+
+	T getItem() {
+		return item;
+	}
+}
