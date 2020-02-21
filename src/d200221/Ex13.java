5bf560eaab784bc0dabf495958b8ddb6608f2e39
inegg <inegg.apps@gmail.com>
2020-02-21 15:02:17 +0900

    RuntimeException도 던질 수 있음. 그러나 RuntimeException은 의무적으로 예외처리할 방법을 기술할 필요는 없다. (try-catch 블록으로 감싸거나 throws RuntimeException으로 호출한 메서드에게 던지거나)

--- src/d200221/Ex13.java
+++ src/d200221/Ex13.java
@@ -1,18 +1,29 @@
 package d200221;
 
 public class Ex13 {
-	//unchecked Exception 관련된 예제
+	// unchecked Exception 관련된 예제
 	public static void main(String[] args) {
+//		User13 uu = new User13();
+//		uu.setValue(-1);// setValue 메서드를 호출한 뒤 RuntimeException 예외가 발생한다.
+//		//하지만 예외처리를 하지 않아서 프로그램이 강제로 종료된다.
+//		int n = uu.getValue();
+//		System.out.println(n);
+//		System.out.println("end....");
+//	}
 		User13 uu = new User13();
-		uu.setValue(-1);// setValue 메서드를 호출한 뒤 RuntimeException 예외가 발생한다.
-		//하지만 예외처리를 하지 않아서 프로그램이 강제로 종료된다.
-		int n = uu.getValue();
-		System.out.println(n);
+		try {
+			uu.setValue(-1);// setValue 메서드를 호출한 뒤 RuntimeException 예외가 발생한다.
+			// 하지만 예외처리를 하지 않아서 프로그램이 강제로 종료된다.
+			int n = uu.getValue();
+			System.out.println(n);
+		} catch (Exception e) {
+			System.out.println(e );
+		}
 		System.out.println("end....");
 	}
 }
 
-class User13{
+class User13 {
 	private int value;
 
 	public int getValue() {
@@ -20,12 +31,12 @@ class User13{
 	}
 
 	public void setValue(int value) {
-		if(value<0) {
-			//unchecked Exception 종류인 RuntimeException은 메서드에 throws를 반드시 명시할 필요가 없다.
-			//따라서 반드시 이 메서드를 부른 메서드에서도 try-catch 구문을 사용할 필요가 없다.
+		if (value < 0) {
+			// unchecked Exception 종류인 RuntimeException은 메서드에 throws를 반드시 명시할 필요가 없다.
+			// 따라서 반드시 이 메서드를 부른 메서드에서도 try-catch 구문을 사용할 필요가 없다.
 			throw new RuntimeException("0 이상만 가능합니다. ");
 		}
 		this.value = value;
 	}
-	
+
 }
