c5e0af57384b7928b771f4df529d7242b0f49b8b
inegg <inegg.apps@gmail.com>
2020-02-25 20:10:13 +0900

    Set을 이용하여 로또 번호 생성

---
+++ src/Ex11.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Ex11 {
+	public static void main(String[] args) {
+		//·Î¶Ç
+		Set<Integer> set = new TreeSet<Integer>();
+		Random rd = new Random();
+		int num;
+		
+		while(set.size()<6) {
+			num = rd.nextInt(45)+1;
+			set.add(num);
+		}
+		System.out.println(set);
+	}
+}
