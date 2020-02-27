5b607ed8ac2d4baf7c15f0be0d1d7b1d284cfcb8
inegg <inegg.apps@gmail.com>
2020-02-27 09:20:08 +0900

    Shuffle 알고리즘 개선

--- src/d200227/Ex3.java
+++ src/d200227/Ex3.java
@@ -19,13 +19,15 @@ public class Ex3 {
 		String s;
 		int n;
 
-		//Shuffle 알고리즘 구현
+		// Shuffle 알고리즘 구현
 		for (int i = 0; i < list.size(); i++) {
 			n = (int) (Math.random() * list.size());
 			if (n != i) {// 현재 배열의 인덱스i값과 이동시키려는 인덱스 n값이 같지 않으면
 				s = list.get(i);
 				list.set(i, list.get(n));
 				list.set(n, s);
+			} else {// i와 n이 같은 자리이면 의미없으니까 다시 한 번 더 실행하도록 유도하면 셔플 알고리즘이 좀 더 수행된다.
+				i--;
 			}
 		}
 		System.out.println(list);
