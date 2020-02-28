c4d872735688a35b072abb9a96b36b6882e1b819
inegg <inegg.apps@gmail.com>
2020-02-28 09:47:08 +0900

    같은 도서 대여 제한하기  반납 후 계속 반납됨  반납할 때 코드번호가 틀려도 오류메시지 없이 반납일 입력으로 넘어가는 현상

--- src/bookEx/RentalService.java
+++ src/bookEx/RentalService.java
@@ -4,78 +4,78 @@ import java.util.Calendar;
 import java.util.Scanner;
 
 public class RentalService {
-	private Scanner sc=new Scanner(System.in);
+	private Scanner sc = new Scanner(System.in);
 	private Rental rental = new RentalImpl();
 	private Book book;
-	
+
 	public RentalService(Book book) {
 		this.book = book;
 	}
-	
+
 	public void bookRental(MemberVO loginMember) {
 		System.out.println("도서 대여...\n");
-		
+
 		String code, rentalDate;
 		char ch;
-		
+
 		try {
 			System.out.print("대여할 도서 코드 ? ");
 			code = sc.next();
 			RentalVO rvo = rental.readRental(code);
-			if(rvo!=null) {
+			if (rvo != null) {
 				System.out.println("도서가 대여 중입니다.\n");
 				return;
 			}
-			
+
 			BookVO bvo = book.readBook(code);
-			if(bvo == null) {
+			if (bvo == null) {
 				System.out.println("등록된 도서가 아닙니다.\n");
 				return;
 			}
-			
-			System.out.print(bvo.getSubject()+" 도서를 대여 하시겠습니까[Y/N] ? ");
+
+			System.out.print(bvo.getSubject() + " 도서를 대여 하시겠습니까[Y/N] ? ");
 			ch = sc.next().charAt(0);
-			
-			if(ch != 'Y' && ch != 'y') {
+
+			if (ch != 'Y' && ch != 'y') {
 				return;
 			}
-			
+
 			rentalDate = String.format("%tF", Calendar.getInstance());
-			
-			System.out.println("::::::"+loginMember);
+
+			System.out.println("::::::" + loginMember);
 			RentalVO vo = new RentalVO();
 			vo.setId(loginMember.getId());
 			vo.setCode(code);
 			vo.setRentalDate(rentalDate);
-			
+
 			rental.insertRental(vo);
-			
+
 			System.out.println("대여가 완료 되었습니다.\n");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void bookReturn(MemberVO loginMember) {
 		System.out.println("도서 반납...\n");
-		
-		String code, rentalDate;
-		
+
+		String code, returnDate;
+
 		try {
 			System.out.print("반납할 도서 코드 ? ");
 			code = sc.next();
-			
+
 			RentalVO vo = rental.readRental(code);
-			if(vo == null || ! vo.getId().equals(loginMember.getId())) {
+			if (vo == null || !vo.getId().equals(loginMember.getId())) {
 				System.out.println("대여 코드가 아닙니다.\n");
 				return;
 			}
-			
-			rentalDate = String.format("%tF", Calendar.getInstance());
-			vo.setRentalDate(rentalDate);
-			
+
+			returnDate = String.format("%tF", Calendar.getInstance());
+			vo.setReturnDate(returnDate);
+
 			System.out.println("반납이 완료 되었습니다.\n");
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
