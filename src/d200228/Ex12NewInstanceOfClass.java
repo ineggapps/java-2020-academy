6cc0090d48b0ca2de374e5fbcbd1068bc33e5f6f
inegg <inegg.apps@gmail.com>
2020-02-28 12:32:01 +0900

    Class클래스로 객체 생성하기

---
+++ src/d200228/Ex12NewInstanceOfClass.java
@@ -0,0 +1,32 @@
+package d200228;
+
+public class Ex12NewInstanceOfClass {
+	public static void main(String[] args) {
+		String clsName = "d200228.User12";//패키지명.클래스명으로 꼭 패키지명을 언급해야 함.
+		try {
+			Class<?> cls = Class.forName(clsName);
+			//객체 생성 또 다른 방법 (new 쓰지 않고)
+			Object oo = cls.newInstance();
+
+			//다운 캐스팅하여 사용하기
+			User12 uu = (User12)oo;
+			int s= uu.add(10, 5);
+			uu.print("합", s);
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+class User12 {
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	public void print(String title, int s) {
+		System.out.println(title + ":" + s);
+	}
+
+}
